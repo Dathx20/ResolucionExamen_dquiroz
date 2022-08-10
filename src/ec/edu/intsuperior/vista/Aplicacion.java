89adf00c9d7e26d5ebb34aee588c6158e3361172
DAVID <DAVID@DESKTOP-TC0PPM0>
2022-08-09 19:29:02 -0500

    Version 1.1 Taller_dquiroz

---
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,16 @@
+package ec.edu.intsuperior.vista;
+import ec.edu.intsuperior.modelo.Persona;
+
+/**
+ *
+ * @author DAVID
+ */
+public class Aplicacion {
+
+    public static void main(String[] args) {
+        
+   //Persona p1 = new Persona("1751531466", "David", "Quiroz","20/08/2003");
+   
+    }
+
+}
